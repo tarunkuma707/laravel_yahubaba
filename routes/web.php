a78b48ac484e0c2469d1ab10e7456740876e469d
apnaproject <tarunkuma07@gmail.com>
2025-02-17 11:49:50 +0530

    Basic Routes

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/post', function () {
-    return view('post');
-});
+// Route::get('/post', function () {
+//     return view('post');
+// });
 
-Route::get('/post/firstpost', function () {
-    return view('firstpost');
-});
+// Route::get('/post/firstpost', function () {
+//     return view('firstpost');
+// });
+
+Route::get("/post/{id?}/comment/{commentid?}",function(string $id=null,string $commentId=null){
+    if($id){
+        return "<h1>POST $id</h1>
+                <h2>Comment Id $commentId</h2>";
+    }
+    else{
+        echo "<h1>No ID Found</h1>";
+    }
+})->whereNumber("id")->whereNumber("commentid");
 
 //Route::view('post','/post');
 
