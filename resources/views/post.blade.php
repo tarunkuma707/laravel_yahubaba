9ecc272690e0b96547966681e172e45f35f37591
apnaproject <tarunkuma07@gmail.com>
2025-02-18 11:17:29 +0530

    Route Redirect

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,1 +1,3 @@
 <h1>Our Post Page</h1>
+<a href="/">Home</a>
+<a href="/about">About</a>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test',function(){
+    return view('about');
+});
+
 // Route::get('/post', function () {
 //     return view('post');
 // });
@@ -14,18 +18,14 @@ Route::get('/', function () {
 //     return view('firstpost');
 // });
 
-Route::get("/post/{id?}/comment/{commentid?}/id/{otherid?}/song/{songvalue?}",function(string $id=null,string $commentId=null, $otherid=null){
-    if($id){
-        return "<h1>POST $id</h1>
-                <h2>Comment Id $commentId</h2>";
-    }
-    else{
-        echo "<h1>No ID Found</h1>";
-    }
-})->where("id",'[a-zA-Z]+')->whereAlphaNumeric("commentid")->whereAlpha('otherid')->whereIn('song',['romantic','sad','devotional']);
+Route::get("/page/post",function(){
+    return view('post');
+    });
 
 //Route::view('post','/post');
 
 // Route::get('/hello', function () {
 //     return view('post');
 // });
+
+Route::redirect('/about','/test', 301);
