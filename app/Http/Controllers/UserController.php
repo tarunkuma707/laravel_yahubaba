20c585bde55089f57ece21759097e721ba987f49
apnaproject <tarunkuma07@gmail.com>
2025-02-23 13:02:06 +0530

    Laravel SQL Raw Queries CRUD Operation Lecture26 Yahoobaba V2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,8 +17,14 @@ class UserController extends Controller
         //                     where id=:id',['id'=>2], "and age=:age",['age'=>18]);
         //$insertData     =   DB::insert("INSERT INTO users (name,age,email,city) VALUES (?,?,?,?)",["TTT",33,"ab1c@gmail.com","Kangra"]);
         //$udpatedata =   DB::update("UPDATE users SET email='abc@gmail.com' WHERE id=?",[2]);
-        $udpatedata =   DB::unprepared("DELETE from users where id=10");
+        //$udpatedata =   DB::unprepared("DELETE from users where id=10");
         //$deletedta      =   DB::delete("DELETE from users where id=?",[8]);
-        //return $allusers;
+        $users          =   DB::table('users')
+                            ->selectRaw('count(*) as student_count, age')
+                            //->whereRaw('age = ? and name like ?', [18,'s%'])
+                            ->groupBy('age')
+
+                            ->get();
+        return $users;
     }
 }
